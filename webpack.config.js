14043ce6994ad26c7d891f17791af71ccbeb75c5
Ellis Tran <41351491+EllisTran@users.noreply.github.com>
2023-01-15 12:40:34 -0500

    Feture: Add eslint, gitignore, prettier, and other settings that I like

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,14 @@ module.exports = {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      }
     ]
   },
   devtool: prod ? undefined : 'source-map',
