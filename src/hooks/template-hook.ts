8fec5b3a1d26c424786c3dd88e81472e85250f03
Ellis Tran <41351491+EllisTran@users.noreply.github.com>
2023-01-16 15:11:17 -0500

    Feature: Add template hooks, and context

---
+++ src/hooks/template-hook.ts
@@ -0,0 +1,7 @@
+// import { useContext } from 'react';
+// import BookContext from '../context/BookContext';
+// const useBooksContext = () => {
+//     return useContext(BookContext);
+// };
+
+// export default useBooksContext;
