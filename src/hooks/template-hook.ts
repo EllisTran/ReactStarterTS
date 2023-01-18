6a6908bffb54b8d73b728da7ff6692006a95459e
Ellis Tran <41351491+EllisTran@users.noreply.github.com>
2023-01-17 23:52:00 -0500

    Add directories componentnts, and pages

--- src/hooks/template-hook.ts
+++ src/hooks/template-hook.ts
@@ -1,7 +1,1 @@
-// import { useContext } from 'react';
-// import BookContext from '../context/BookContext';
-// const useBooksContext = () => {
-//     return useContext(BookContext);
-// };
 
-// export default useBooksContext;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './pages/App';
 
-const el = document.getElementById('app-root') as HTMLElement;
+const el = document.getElementById('root') as HTMLElement;
 
 const root = ReactDOM.createRoot(el);
 
